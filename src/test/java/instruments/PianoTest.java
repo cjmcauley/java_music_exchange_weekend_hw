f6f620d71adf263020cbf15dff0499f2af2d46e7
cjmcauley <christopher.mcauley@gmail.com>
2019-09-13 13:51:45 +0100

    piano tests passing

---
+++ src/test/java/instruments/PianoTest.java
@@ -0,0 +1,55 @@
+package instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+import properties.InstrumentFamily;
+import properties.InstrumentType;
+
+import static org.junit.Assert.assertEquals;
+
+public class PianoTest {
+
+    Piano piano;
+
+    @Before
+    public void setUp() {
+        piano = new Piano("Steinway", InstrumentFamily.PERCUSSION, "Black", "Wood", 800, 1950, 88);
+    }
+
+    @Test
+    public void canGetModel() {
+        assertEquals("Steinway", piano.getModel());
+    }
+
+    @Test
+    public void canGetInstrumentFamily() {
+        assertEquals(InstrumentFamily.PERCUSSION, piano.getFamily());
+    }
+
+    @Test
+    public void canGetInstrumentColour() {
+        assertEquals("Black", piano.getColour());
+    }
+
+    @Test
+    public void canGetInstrumentMaterial() {
+        assertEquals("Wood", piano.getMaterial());
+    }
+
+    @Test
+    public void canGetInstrumentPurchasePrice() {
+        assertEquals(800, piano.getPurchasePrice());
+    }
+
+
+    @Test
+    public void canGetInstrumentRetailPrice() {
+        assertEquals(1950, piano.getRetailPrice());
+    }
+
+    @Test
+    public void canGetNoOfKeys() {
+        assertEquals(88, piano.getKeys());
+    }
+
+}
