aef319411a3ac089d8924ecfb1f11d1dcb49c293
cjmcauley <christopher.mcauley@gmail.com>
2019-09-13 13:42:30 +0100

    drum tests passing

---
+++ src/test/java/instruments/DrumsTest.java
@@ -0,0 +1,60 @@
+package instruments;
+
+import org.junit.Before;
+import org.junit.Test;
+import properties.InstrumentFamily;
+import properties.InstrumentType;
+
+import static org.junit.Assert.assertEquals;
+
+public class DrumsTest {
+
+    Drums drums;
+
+    @Before
+    public void setUp() {
+        drums = new Drums("Tama", InstrumentFamily.PERCUSSION, "Red", "Wood", 299, 650, 7, InstrumentType.ACOUSTIC);
+    }
+
+    @Test
+    public void canGetModel() {
+        assertEquals("Tama", drums.getModel());
+    }
+
+    @Test
+    public void canGetInstrumentFamily() {
+        assertEquals(InstrumentFamily.PERCUSSION, drums.getFamily());
+    }
+
+    @Test
+    public void canGetInstrumentColour() {
+        assertEquals("Red", drums.getColour());
+    }
+
+    @Test
+    public void canGetInstrumentMaterial() {
+        assertEquals("Wood", drums.getMaterial());
+    }
+
+    @Test
+    public void canGetInstrumentPurchasePrice() {
+        assertEquals(299, drums.getPurchasePrice());
+    }
+
+
+    @Test
+    public void canGetInstrumentRetailPrice() {
+        assertEquals(650, drums.getRetailPrice());
+    }
+
+    @Test
+    public void canGetNoOfDrums() {
+        assertEquals(7, drums.getNoOfDrums());
+    }
+
+
+    @Test
+    public void canGetInstrumentType() {
+        assertEquals(InstrumentType.ACOUSTIC, drums.getType());
+    }
+}
